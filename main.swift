bbeee164f02bda3758feab778997661aa9480946
Carowl Carvalho <62719696+CarowlCarvalho@users.noreply.github.com>
2021-02-20 16:18:19 -0300

    Update main.swift

--- main.swift
+++ main.swift
@@ -14,6 +14,8 @@ print(nome + " " + sobrenome)
 //interpolação
 print("\(nome) \(sobrenome) ")
 
+print("----------------------------------------------------")
+
 /*Exercícios sessão 2
 1 - Crie uma variável com o nome da cidade onde você nasceu
 2 - Crie uma constante com o ano em que você nasceu
@@ -27,6 +29,8 @@ let ano = 1993
 print( "Eu nasci na cidade de",cidade,", no ano de", ano)
 print ("Eu nasci na cidade de " + cidade + ", no ano de " + String(ano))
 print("Eu nasci na cidade de \(cidade), no ano de\(ano)")
+
+print("----------------------------------------------------")
 
 
 /*Seção 3
@@ -87,6 +91,8 @@ let numerim2 : Float = 9.6
 
 var amanhaESabado1: Bool = false
 
+print("----------------------------------------------------")
+
 
 // 1- Operadores aritméticos:
 // ( + , - , * , / )
@@ -117,6 +123,8 @@ var altura = 1.65
 
 Double(idade) * altura
 
+print("----------------------------------------------------")
+
 
 //Seção 4
 
@@ -134,6 +142,8 @@ filmesQueEuGosto.count
 
 temperos.count
 
+print("----------------------------------------------------")
+
 /*Arrays Exercícios
 1- Crie um Array explicitamente com o Data Type Double, que contenha 5 elementos
 2- Adicione mais 3 elementos através do método Append
@@ -150,3 +160,65 @@ alturas.count
 alturas.append(Double(alturas.count))
 
 print(alturas)
+
+print("----------------------------------------------------")
+
+// O que é o tal do index?
+
+//Arrays
+
+var emojis : [String] = ["feliz", "anjo", "bravo", "sono", "óculos"]
+
+//Mostrando o 1º e último termo do array
+print(emojis[0])
+print(emojis[4])
+
+print(emojis.first)
+print(emojis.last)
+
+//Alterando um termo do array
+emojis[emojis.count - 1] = "coração"
+print(emojis)
+
+emojis[2] = "cachorro"
+print(emojis)
+
+//Insert e Remove
+
+emojis.insert("lol", at: 2)
+print(emojis)
+
+emojis.remove(at: 2)
+print(emojis)
+
+//Contains e Index of
+
+print(emojis.contains("chorando"))
+
+print(emojis.index(of: "feliz"))
+print("----------------------------------------------------")
+/*Exercícios
+1- Crie um array de Strings com 5 elementos
+2- Retorne o primeiro elemento da sua array com o método .first, e o último com .last
+3- Atualize o 4º elemento sem alterar o código inicial
+4- Remova o penúltimo elemento da sua array
+5- Insira um novo elemento no 2º index
+6- Use o método .contains para verificar se existe um elemento na sua array
+7-  Utilize o método .index(of: )
+para verificar o index de um elemento*/
+
+var birds : [String] = ["pombo", "beija-flor", "bem-te-vi", "andorinha", "sabiá"]
+
+print(birds)
+print(birds.first)
+print(birds.last)
+birds[4] = "pardal"
+print(birds)
+birds.remove(at: 3)
+print(birds)
+birds.insert("joão de barro", at: 2)
+print(birds)
+print(birds.contains("canário"))
+print(birds.index(of: "bem-te-vi"))
+
+print("----------------------------------------------------")
